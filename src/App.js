ffa3143b4153dd5d83daee01a518bf0f2480e721
Sjoerd Vink <sjoerdvink@icloud.com>
2021-02-18 09:31:18 +0100

    repo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import { Header } from "./components";
 
 function App() {
   return (
-    <div className='App'>
+    <div className='app'>
       <h1>amazon</h1>
+      <Header />
+      {/* Home */}
     </div>
   );
 }
